2f00fcaa4cd72fda6e0afaaa9a0b588bdc230781
Rezamdan <114395550+Rezamdan@users.noreply.github.com>
2022-09-26 16:59:02 +0700

    Add files via upload

---
+++ TugasPBOs1RezaRamdaniI.java
@@ -0,0 +1,13 @@
+package tugaspbos1rezaramdanii;
+public class TugasPBOs1RezaRamdaniI {
+
+    public static void main(String[] args) {
+
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("dan sangat menyenangkan");
+
+    }
+    
+}
+
